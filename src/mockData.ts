51a53481a5f4bd8b7fba81464f099be92c8269b4
Shummik <gllip@mail.ru>
2024-10-05 15:58:06 +0300

    добавил основные страницы

---
+++ src/mockData.ts
@@ -0,0 +1,9 @@
+export const profileData = {
+    email: 'mail@example.com',
+    login: 'jhonson',
+    firstname: 'Jhonatan',
+    lastname: 'Maklein',
+    phone: '+7(987)6543210',
+    displayName: 'Jhon',
+    password: 'der_password'
+}
